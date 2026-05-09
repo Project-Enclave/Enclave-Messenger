ec62315ab43b83eaa3e9d57335280989986071a0
Chinglen2080 <106335829+Chinglen2080@users.noreply.github.com>
2026-05-09 16:41:40 +0530

    chore(storage): export PeerStore

--- core/storage/__init__.py
+++ core/storage/__init__.py
@@ -2,5 +2,6 @@ from .config_store import ConfigStore
 from .key_store import KeyStore
 from .chat_store import ChatStore
 from .log_store import LogStore
+from .peer_store import PeerStore
 
-__all__ = ["ConfigStore", "KeyStore", "ChatStore", "LogStore"]
+__all__ = ["ConfigStore", "KeyStore", "ChatStore", "LogStore", "PeerStore"]
